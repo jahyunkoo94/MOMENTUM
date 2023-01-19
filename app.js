4fc63ac540fdf0beb026171cfb30f84479eaa7db
jahyunkoo94 <81736745+jahyunkoo94@users.noreply.github.com>
2023-01-19 13:46:56 +0900

    update

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const age = prompt("How old are you?");
+const title = document.querySelector(".title");
 
-console.log(age);
+function handleTitleEnter() {
+  title.innerText = "Mouse is here!";
+  title.style.color = "blue";
+}
+
+function handleTitleLeave() {
+  title.innerText = "Mouse is gone!";
+  title.style.color = "teal";
+}
+
+function handleWindowResize() {
+  title.innerText = "you just resize!";
+  title.style.color = "tomato";
+}
+
+function handleMouseContextMenu() {
+  title.innerText = "That was a right click!";
+  title.style.color = "purple";
+}
+
+title.addEventListener("mouseenter", handleTitleEnter);
+title.addEventListener("mouseleave", handleTitleLeave);
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("contextmenu", handleMouseContextMenu);
